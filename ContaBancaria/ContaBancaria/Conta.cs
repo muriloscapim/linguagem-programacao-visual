23c3562a999c784fded4a1f9d47fc7f5da50ea94
Murilo Scapim <murilo.scapim@outlook.com>
2020-03-28 14:57:42 -0300

    Aula POO

---
+++ ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        public bool Saca(double valor)
+        {
+            if(this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+            c.numero = 1;
+            c.titular = "Victor";
+            c.saldo = 100.0;
+            bool deuCerto = c.Saca(10.0);
+
+            Console.WriteLine(deuCerto ? "Saque realizado com sucesso!"
+                : "Saldo insuficiente!");
+        
+            Conta c1 = new Conta();
+            c1.numero = 2;
+            c1.titular = "João";
+            c1.saldo = 200.0;
+            c1.Saca(100.0);
+
+            Console.WriteLine(c.saldo);
+            Console.WriteLine(c1.saldo);
+        }
+    }
+}
